f058d41f37a5e3aa22292d6b96675ca5635975d4
Rupa <rupa211984@gmail.com>
2019-11-12 21:07:46 -0800

    Done

--- app.rb
+++ app.rb
@@ -4,6 +4,19 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :index
+    end
 
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height])
+      @ships = Ship.create_ships(params[:pirate][:ships])
+
+      erb :'pirates/show'
+    end
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,10 @@
 class Pirate
+  attr_accessor :name, :weight, :height, :ships
+
+  def initialize(name, weight, height)
+    @name = name
+    @weight = weight
+    @height = height
+    @ships = []
+  end
 end
